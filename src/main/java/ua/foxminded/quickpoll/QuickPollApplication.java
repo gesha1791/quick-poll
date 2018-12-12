9a0a8c6a83472afe44cce06f5b0b7c22a74506d4
Ievgen Chaplinskiy <geshachapik@gmail.com>
2018-12-12 16:17:25 +0200

    change indent

--- src/main/java/ua/foxminded/quickpoll/QuickPollApplication.java
+++ src/main/java/ua/foxminded/quickpoll/QuickPollApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class QuickPollApplication {
-				public static void main(String[] args) {
-								SpringApplication.run(QuickPollApplication.class, args);
-				}
+    public static void main(String[] args) {
+        SpringApplication.run(QuickPollApplication.class, args);
+    }
 }
